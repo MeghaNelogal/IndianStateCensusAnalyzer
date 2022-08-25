282571f37a9d69e1f607b456569c9dd54dd27bda
Your Name <you@example.com>
2022-08-25 23:00:46 +0530

    [Megha] Add: Load the Indian State Code Information from a CSV File

--- IndianStateCensusAnalyzer/IndianStateCensusAnalyzer/StateCesusData.cs
+++ IndianStateCensusAnalyzer/IndianStateCensusAnalyzer/StateCesusData.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace IndianStateCensusAnalyzer
 {
-    internal class StateCesusData
+    public class StateCesusData
     {
+        string state;
+        string population;
+        string area;
+        string density;
+        string serialNumber;
+        string stateName;
+        string tin;
+        string stateCode;
+
+        public StateCesusData(CensusStateCode censusStateCode)
+        {
+            this.stateName = censusStateCode.StateName;
+            this.serialNumber = censusStateCode.SerialNumber;
+            this.tin = censusStateCode.Tin;
+            this.stateCode = censusStateCode.StateCode;
+        }
+
+        public StateCesusData(string state, string population, string area, string density)
+        {
+            this.state = state;
+            this.population = population;
+            this.area = area;
+            this.density = density;
+        }
     }
 }
+
+
